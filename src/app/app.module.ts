f5b053253e2374591d2bc5161a903f5186ecb5d3
Roosevelt Franklin <roosevelt_franklin@hotmail.com>
2023-08-06 12:19:44 -0300

    Favoritos e formatação de moeda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,47 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { QuemSomosComponent } from './quem-somos/quem-somos.component';
 import { ContatoComponent } from './contato/contato.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { DetalhesComponent } from './detalhes/detalhes.component';
+import { RodapeComponent } from './rodape/rodape.component';
+
+import ptBr from '@angular/common/locales/pt';
+import { registerLocaleData, NgFor } from '@angular/common';
+
+registerLocaleData(ptBr);
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     QuemSomosComponent,
-    ContatoComponent
+    ContatoComponent,
+    CabecalhoComponent,
+    DetalhesComponent,
+    RodapeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    NgFor
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/detalhes/detalhes.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-detalhes',
+  templateUrl: './detalhes.component.html',
+  styleUrls: ['./detalhes.component.scss']
+})
+export class DetalhesComponent implements OnInit  {
+
+  imovelId: string;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.imovelId = params.get('id') ?? '';
+    });
+  }
+}
